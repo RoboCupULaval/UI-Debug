617c55909f0943fbdcb9f591d4870a62b2948853
Julien Becirovski <j.becirovski@gmail.com>
2016-06-08 16:19:11 -0400

    add data out model

---
+++ Model/DataOutModel.py
@@ -0,0 +1,14 @@
+# Under MIT License, see LICENSE.txt
+
+from Communication.UDPCommunication import UDPSending
+
+__author__ = 'RoboCupULaval'
+
+
+class DataOutModel(object):
+    def __init__(self):
+        self._udp_sender = UDPSending()
+        self.target = (0, 0)
+
+    def send_tactic(self, id_bot, tactic, target=(0, 0), goal=(0, 0)):
+        self._udp_sender.send_message({'tactic': {'id': id_bot, 'tactic': tactic, 'target': target, 'goal': goal}})
