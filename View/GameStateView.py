885e9145ae326caf9d27efd4c839facf8695e7eb
pturgeon <philippe.turgeon.4@ulaval.ca>
2017-05-14 20:59:02 -0400

    Upgrade en cours de la vue de l'état des robots -- marche pas encore : WORK IN PROGRESS

--- View/GameStateView.py
+++ View/GameStateView.py
@@ -2,8 +2,10 @@
 
 import logging
 from threading import Thread
-from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QScrollArea, QPlainTextEdit, QPushButton, QVBoxLayout, QHBoxLayout, QGroupBox, QTextEdit
+from PyQt5.QtCore import Qt, QRect
+from PyQt5 import QtGui
+
 
 __author__ = 'RoboCupULaval'
 
@@ -33,13 +35,13 @@ class GameStateView(QWidget):
         #self.hide()
         self._logger.debug('CONSTRUCT: ... End')
 
-    def _get_style_sheet(self, is_yellow=None, bold=False, color='black'):
+    def _get_style_sheet(self, team=None, bold=False, color='black'):
         param_sheet = ' ; '.join(['color:{}'.format(color),
                                 'border-style: solid',
                                 'border-color: black',
                                 'border-width: 1px'])
-        if is_yellow is not None:
-            if is_yellow:
+        if team is not None:
+            if team == 'yellow':
                 color = '#ffff88'
             else:
                 color = '#8888ff'
@@ -65,55 +67,101 @@ class GameStateView(QWidget):
         self._layout.setVerticalSpacing(0)
         self._layout.setHorizontalSpacing(0)
 
+        self._active_team = 'blue'  # TODO (pturgeon):
+
         # Main header
-        label_blue_team = QLabel('Équipe Bleue')
+        label_blue_team = QLabel(self._active_team+' team')
         label_blue_team.setMargin(5)
         label_blue_team.setAlignment(Qt.AlignCenter)
-        label_blue_team.setStyleSheet(self._get_style_sheet(is_yellow=False))
+        label_blue_team.setStyleSheet(self._get_style_sheet(self._active_team))
         self._layout.addWidget(label_blue_team, 1, 1)
 
         self._label_strat_blue = QLabel('None')
         self._label_strat_blue.setMargin(5)
         self._label_strat_blue.setAlignment(Qt.AlignCenter)
-        self._label_strat_blue.setStyleSheet(self._get_style_sheet(is_yellow=False))
+        self._label_strat_blue.setStyleSheet(self._get_style_sheet(self._active_team))
         self._layout.addWidget(self._label_strat_blue, 1, 2, 1, 3)
 
-        label_yellow_team = QLabel('Équipe Jaune')
-        label_yellow_team.setMargin(5)
-        label_yellow_team.setAlignment(Qt.AlignCenter)
-        label_yellow_team.setStyleSheet(self._get_style_sheet(is_yellow=True))
-        self._layout.addWidget(label_yellow_team, 1, 5)
+        #label_yellow_team = QLabel('Équipe Jaune')
+        #label_yellow_team.setMargin(5)
+        #label_yellow_team.setAlignment(Qt.AlignCenter)
+        #label_yellow_team.setStyleSheet(self._get_style_sheet(is_yellow=True))
+        #self._layout.addWidget(label_yellow_team, 1, 5)
 
-        self._label_strat_yellow = QLabel('None')
-        self._label_strat_yellow.setMargin(5)
-        self._label_strat_yellow.setAlignment(Qt.AlignCenter)
-        self._label_strat_yellow.setStyleSheet(self._get_style_sheet(is_yellow=True))
-        self._layout.addWidget(self._label_strat_yellow, 1, 6, 1, 3)
+        #self._label_strat_yellow = QLabel('None')
+        #self._label_strat_yellow.setMargin(5)
+        #self._label_strat_yellow.setAlignment(Qt.AlignCenter)
+        #self._label_strat_yellow.setStyleSheet(self._get_style_sheet(is_yellow=True))
+        #self._layout.addWidget(self._label_strat_yellow, 1, 6, 1, 3)
 
         # Sub header
-        for i, header in enumerate(['Robot', 'Tactique', 'Action', 'Target']*2, start=1):
+        for i, header in enumerate(['Robot', 'Tactique', 'Action', 'Target'], start=1):
             label = QLabel(header)
             label.setAlignment(Qt.AlignCenter)
             label.setStyleSheet(self._get_style_sheet(bold=True))
             self._layout.addWidget(label, 2, i)
 
-        list_active_robots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]  # TODO (pturgeon): Créer variable globale
-        #list.remove(2)
+        self._list_active_robots = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]  # TODO (pturgeon): Créer variable globale
+
         # id robot
-        for i in range(len(list_active_robots)):
-            for j in range(2):
-                label = QLabel(str(list_active_robots[i]))
-                label.setAlignment(Qt.AlignCenter)
-                label.setStyleSheet(self._get_style_sheet(bold=True))
-                self._layout.addWidget(label, 3 + i, 1 + 4 * j)
+        for i in range(len( self._list_active_robots)):
+            label = QLabel(str( self._list_active_robots[i]))
+            label.setAlignment(Qt.AlignCenter)
+            label.setStyleSheet(self._get_style_sheet(bold=True))
+            self._layout.addWidget(label, 3 + i, 1)
 
         # None value everywhere else
-        for i in range(len(list_active_robots)):
-            for j in [1, 2, 3, 5, 6, 7]:
+        for i in range(len( self._list_active_robots)):
+            for j in [1, 2, 3]:
                 label = QLabel('None')
                 label.setAlignment(Qt.AlignCenter)
                 label.setStyleSheet(self._get_style_sheet(color='#222222'))
                 self._layout.addWidget(label, 3 + i, 1 + j)
+
+
+
+
+
+        '''test'''
+        '''http://www.programcreek.com/python/example/82631/PyQt5.QtWidgets.QScrollArea'''
+        '''http://stackoverflow.com/questions/9624281/how-to-associate-a-horizontal-scrollbar-to-multiple-groupbox'''
+        self.setWindowModality(Qt.ApplicationModal)
+        self.setMaximumSize(400, 230)
+        self.setMinimumSize(400, 230)
+        self.resize(400, 230)
+
+        self.scrollArea = QScrollArea(self)
+        self.scrollArea.setGeometry(QRect(5, 5, 390, 190))
+        self.scrollArea.setWidgetResizable(True)
+
+        self.plainTextEdit = QPlainTextEdit()
+        self.plainTextEdit.setGeometry(QRect(0, 0, 390, 190))
+        self.plainTextEdit.setPlainText('toto')
+
+        self.scrolllayout = QVBoxLayout()
+        self.scrollwidget = QWidget()
+        self.scrollwidget.setLayout(self.scrolllayout)
+        self.groupboxes = []  # Keep a reference to groupboxes for later use
+        for i in range(8):  # 8 groupboxes with textedit in them
+            groupbox = QGroupBox('%d' % i)
+            grouplayout = QHBoxLayout()
+            grouptext = QTextEdit()
+            grouplayout.addWidget(grouptext)
+            groupbox.setLayout(grouplayout)
+            self.scrolllayout.addWidget(groupbox)
+            self.groupboxes.append(groupbox)
+
+        self.scrollArea.setWidget(self.scrollwidget)
+
+        self.acceptButton = QPushButton(self)
+        self.acceptButton.setGeometry(QRect(280, 200, 100, 25))
+        self.acceptButton.setText("Ok")
+
+        self.rejectButton = QPushButton(self)
+        self.rejectButton.setGeometry(QRect(160, 200, 100, 25))
+        self.rejectButton.setText("Cancel")
+        '''test fini'''
+
 
     def init_loop(self):
         self._logger.debug('INIT: Robot State Loop')
@@ -142,9 +190,42 @@ class GameStateView(QWidget):
         while True:
             robot_state = self._ctrl.waiting_for_robot_state()
             self._logger.debug('RUN: Received robot state')
-            print(robot_state)
             if robot_state is not None:
-                for team in robot_state.keys():
+                #_active_team = 'blue' # TODO (pturgeon): pouvoir afficher l'équipe jaune dans le tableau
+                self._list_active_robots = []
+                for id in robot_state[self._active_team].keys():
+                    if robot_state[self._active_team][id]['action'] is not None:
+                        self._list_active_robots.append(id)
+                self._list_active_robots = sorted(self._list_active_robots)
+                print(self._list_active_robots)
+
+                # pour chaque ligne
+                for i in range(len(self._list_active_robots)):
+                    line = 3 + i
+                    id = self._list_active_robots[i]
+                    for state in robot_state[self._active_team][id].keys():
+                        if state == 'action':
+                            col = 3
+                        elif state == 'target':
+                            col = 4
+                        else:
+                            col = 2
+                        object = self._layout.itemAtPosition(line, col)
+                        object.widget().setText(str(robot_state[self._active_team][id][state]))
+                    object = self._layout.itemAtPosition(line, 1)
+                    object.widget().setText(str(self._list_active_robots[i]))
+
+                for line in range(i+4, 15):
+                    for col in range(1, 5):
+                        object = self._layout.itemAtPosition(line, col)
+                        if object is not None:
+                            object.widget().close()
+                        else:
+                            break
+
+
+
+                '''for team in robot_state.keys():
                     if team == 'blue':
                         t_col = 2
                     else:
@@ -167,7 +248,7 @@ class GameStateView(QWidget):
                                                                               line, col))
                                     object.widget().setText(str(robot_state[team][id][state]))
                             else:
-                                self._logger.warn('RUN: NoneType detected at {}, {}'.format(line, col))
+                                self._logger.warn('RUN: NoneType detected at {}, {}'.format(line, col))'''
 
     def show_hide(self):
         self._logger.debug('TRIGGER: Show/Hide')
