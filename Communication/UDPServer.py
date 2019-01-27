24eea5c026c8ef309cc901c2bc69a4c8b052919f
Philippe Babin <philippe.babin@gmail.com>
2019-01-26 21:35:07 -0500

    Remove print

--- Communication/UDPServer.py
+++ Communication/UDPServer.py
@@ -38,7 +38,7 @@ class UDPServer(Thread):
         self.init_logger()
 
     def toggle_debug(self):
-        print(self._logger.getEffectiveLevel())
+        pass
 
     def init_logger(self):
         ch = logging.StreamHandler()
--- View/FieldView.py
+++ View/FieldView.py
@@ -427,7 +427,6 @@ class FieldView(QWidget):
 
     def wheelEvent(self, event):
         """ Gère l'événement de la molette de la souris """
-        print(event.angleDelta().y())
         if event.angleDelta().y() > 0:
             QtToolBox.field_ctrl.zoom(event.pos().x(),
                                       event.pos().y(),
